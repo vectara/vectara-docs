50db78788eae791b3c1082d4139d1432cb037da3
Shane Connelly <shane@eskibars.com>
2023-03-28 21:56:48 -0700

    Update the styling for OAS/API Playground (#33)
    * Add more defaults and examples
    
    * Update styling for OpenAPI spec primarily
    
    * Fix up extraneous comments

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -157,7 +157,17 @@ ${content}
   themeConfig: {
     prism: {
       additionalLanguages: ['java', 'php', 'csharp'],
-    },
+      theme: require('prism-react-renderer/themes/vsLight'),
+      darkTheme: require('prism-react-renderer/themes/dracula'),
+    },/*
+    announcementBar: {
+      id: 'support_us',
+      content:
+        'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+      backgroundColor: '#fafbfc',
+      textColor: '#091E42',
+      isCloseable: false,
+    },*/
     navbar: {
       title: '',
       logo: {
