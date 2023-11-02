9683332076b397675a7326a39a179d115d193d1c
aamirbutt <aamirbutt@gmail.com>
2023-11-02 06:41:50 -0700

    Added Snow Analytics code. (#146)
    * Added Snow Analytics code.
    
    * Addressed one code review comment.
    
    ---------
    
    Co-authored-by: Aamir Butt <aamir@vectara.com>

---
+++ www/static/routeUpdateModule.js
@@ -0,0 +1,10 @@
+
+export function onRouteDidUpdate({ location, previousLocation }) {
+  // Don't execute if we are still on the same page; the lifecycle may be fired
+  // because the hash changes (e.g. when navigating between headings)
+  if (location.pathname !== previousLocation?.pathname) {
+    if (window.snowAnalytics) {
+      window.snowAnalytics.page();
+    }
+  }
+};
