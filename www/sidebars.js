072c12096770e05c0eec303fef5da1d46e3a7197
Paul Wozniczka <25128922+pwoznic@users.noreply.github.com>
2023-11-03 18:12:01 -0500

    Update sidebars.js (#151)
    Move examples into new category since they specific to query operations. This will allow us to expand the code samples in the future, such as Indexing-specific examples

--- www/sidebars.js
+++ www/sidebars.js
@@ -101,13 +101,21 @@ module.exports = {
     {
       type: 'category',
       label: 'Code Samples',
+      collapsed: false,
       items: [
-        'getting-started-samples/RestApiKeyQueries.cs',
-        'getting-started-samples/RestApiKeyQueries.java',
-        'getting-started-samples/queryDataApiKey.php',
-        'getting-started-samples/rest_api_key_queries.py',
-        'getting-started-samples/app.js',
-      ]
+        {
+          type: 'category',
+          label: 'Query Operations Code Samples',
+          collapsed: false,
+          items: [
+            'getting-started-samples/RestApiKeyQueries.cs',
+            'getting-started-samples/RestApiKeyQueries.java',
+            'getting-started-samples/queryDataApiKey.php',
+            'getting-started-samples/rest_api_key_queries.py',
+            'getting-started-samples/app.js',
+          ]
+        }
+     ]
     },
     {
       type: 'category',
