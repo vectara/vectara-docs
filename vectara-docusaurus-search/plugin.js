f84f432e8732c5146b96ee9af0de927876ffe988
Deryk DeGuzman <deryk@vectara.com>
2023-11-08 11:32:02 -0800

    fix: ensure rendering a single instance of search

--- vectara-docusaurus-search/plugin.js
+++ vectara-docusaurus-search/plugin.js
@@ -1,5 +1,20 @@
+const ExecutionEnvironment =
+  require("@docusaurus/ExecutionEnvironment").default;
+
 // The following is a way to circumvent
 // Docusaurus forcibly SSR'ing our client modules.
 if (typeof document !== "undefined") {
   require("./dist/widget");
 }
+
+export default (function () {
+  if (!ExecutionEnvironment.canUseDOM) {
+    return null;
+  }
+  const event = new Event("onRouteUpdated");
+  return {
+    onRouteDidUpdate() {
+      document.dispatchEvent(event);
+    },
+  };
+})();
