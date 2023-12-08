6395512bcf8822fe6a5950ffc408226777c63a12
Aamir Butt <aamir@vectara.com>
2023-12-08 10:53:37 +0500

    Removed rudderstack from docs.

--- www/static/routeUpdateModule.js
+++ www/static/routeUpdateModule.js
@@ -3,8 +3,8 @@ export function onRouteDidUpdate({ location, previousLocation }) {
   // Don't execute if we are still on the same page; the lifecycle may be fired
   // because the hash changes (e.g. when navigating between headings)
   if (location.pathname !== previousLocation?.pathname) {
-    if (window.snowAnalytics) {
-      window.snowAnalytics.page();
+    if (window.analytics) {
+      window.analytics.page();
     }
   }
 };
