f10955c4a6b607a63a1daff7c49cab2b3694aca0
CJ Cenizal <cj@cenizal.com>
2023-12-11 13:04:30 -0800

    Fix bug with BrowserOnly usage.

--- www/src/theme/SearchBar.tsx
+++ www/src/theme/SearchBar.tsx
@@ -3,29 +3,33 @@ import { Search } from "./search/Search";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function SearchBar() {
-  const searchConfig = (window as any).vectara?.plugins?.search;
-
-  if (!searchConfig) {
-    return;
-  }
-
-  const { customerId, apiKey, corpusId } = searchConfig;
-
-  if (!customerId || !apiKey || !corpusId) {
-    console.warn(
-      "Vectara Search: Customer ID, API key, and Corpus ID are required."
-    );
-    return;
-  }
-
   return (
     <BrowserOnly>
-      <Search
-        key={customerId}
-        customerId={customerId}
-        apiKey={apiKey}
-        corpusId={corpusId}
-      />
+      {() => {
+        const searchConfig = (window as any).vectara?.plugins?.search;
+
+        if (!searchConfig) {
+          return;
+        }
+
+        const { customerId, apiKey, corpusId } = searchConfig;
+
+        if (!customerId || !apiKey || !corpusId) {
+          console.warn(
+            "Vectara Search: Customer ID, API key, and Corpus ID are required."
+          );
+          return;
+        }
+
+        return (
+          <Search
+            key={customerId}
+            customerId={customerId}
+            apiKey={apiKey}
+            corpusId={corpusId}
+          />
+        );
+      }}
     </BrowserOnly>
   );
 }
