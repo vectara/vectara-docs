06e17d77db67e8c59a26cc27192fc9bc1dc4ee46
Paul Wozniczka <25128922+pwoznic@users.noreply.github.com>
2024-04-25 10:32:53 -0500

    Update docusaurus.config.js

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -182,14 +182,14 @@ ${content}
       theme: require("prism-react-renderer/themes/vsLight"),
       darkTheme: require("prism-react-renderer/themes/dracula"),
     },
-    announcementBar: {
+    /* announcementBar: {
       id: "genai_launch",
       content:
         '',
       backgroundColor: "#fafbfc",
       textColor: "#091E42",
       isCloseable: true,
-    },
+    }, */
     navbar: {
       title: "",
       logo: {
