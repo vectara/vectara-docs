87a6524592229a2b74e086c74d92d51f9cbcf4b9
CJ Cenizal <cj@cenizal.com>
2024-05-07 09:59:19 -0700

    Fix build error by isolating browser-only logic.

--- www/src/theme/SearchBar.tsx
+++ www/src/theme/SearchBar.tsx
@@ -1,22 +1,26 @@
 import React from "react";
-import { ReactSearch } from "@vectara/react-search";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 const CUSTOMER_ID = "3874164736";
 const CORPUS_ID = "1";
 const API_KEY = "zqt_5usQAFwTytdQHAXn17Iq31OQMA5RrIBWLI7Fwg";
 
-const persistToggleSummaryState = (isSummaryVisible) => {
-  localStorage.setItem("isSummaryVisible", isSummaryVisible ? "true" : "false");
-};
-
-const isSummaryToggleInitiallyEnabled =
-  localStorage.getItem("isSummaryVisible");
-
 export default function SearchBar() {
   return (
     <BrowserOnly>
       {() => {
+        const ReactSearch = require("@vectara/react-search").ReactSearch;
+
+        const persistToggleSummaryState = (isSummaryVisible) => {
+          localStorage.setItem(
+            "isSummaryVisible",
+            isSummaryVisible ? "true" : "false"
+          );
+        };
+
+        const isSummaryToggleInitiallyEnabled =
+          localStorage.getItem("isSummaryVisible");
+
         return (
           <ReactSearch
             key={CUSTOMER_ID}
