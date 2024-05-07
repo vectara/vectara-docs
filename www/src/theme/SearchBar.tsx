1e9a0784119cb636a431688041ec002683c13900
CJ Cenizal <cj@cenizal.com>
2024-05-07 10:10:47 -0700

    Add summarized search (#274)
    * Migrate to React-Search dependency.
    * Persist summary toggle state.
    * Fix build error by isolating browser-only logic.

--- www/src/theme/SearchBar.tsx
+++ www/src/theme/SearchBar.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Search } from "./search/Search";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 
 const CUSTOMER_ID = "3874164736";
@@ -10,12 +9,31 @@ export default function SearchBar() {
   return (
     <BrowserOnly>
       {() => {
+        const ReactSearch = require("@vectara/react-search").ReactSearch;
+
+        const persistToggleSummaryState = (isSummaryVisible) => {
+          localStorage.setItem(
+            "isSummaryVisible",
+            isSummaryVisible ? "true" : "false"
+          );
+        };
+
+        const isSummaryToggleInitiallyEnabled =
+          localStorage.getItem("isSummaryVisible");
+
         return (
-          <Search
+          <ReactSearch
             key={CUSTOMER_ID}
             customerId={CUSTOMER_ID}
             corpusId={CORPUS_ID}
             apiKey={API_KEY}
+            onToggleSummary={(isSummaryVisible) =>
+              persistToggleSummaryState(isSummaryVisible)
+            }
+            isSummaryToggleVisible={true}
+            isSummaryToggleInitiallyEnabled={
+              isSummaryToggleInitiallyEnabled === "true"
+            }
           />
         );
       }}
